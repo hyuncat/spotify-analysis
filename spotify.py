18d273f98a9532523f816b60a5ee3af5e9cd1790
sarah <ssh2198@columbia.edu>
2023-08-24 02:53:14 -0500

    OOP refactoring

--- spotify.py
+++ spotify.py
@@ -1,5 +1,4 @@
 # import libraries
-import os
 import json
 
 import requests
@@ -10,68 +9,108 @@ import spotipy.util as util
 
 from spotifysecrets import *
 
-# credentials
-username = 'nrkjbdqb3gwxlzypjce5vvqra'
-redirect_uri = 'http://localhost:8888/callback'
 
-# dictionary of predefined scopes
-scope = {
-    0: 'user-top-read',
-    1: 'user-read-recently-played',
-    2: 'user-read-currently-playing',
-    3: 'playlist-read-private',
-    4: 'playlist-read-collaborative',
-    5: 'playlist-modify-private',
-    6: 'playlist-modify-public',
-    7: 'user-library-read'
-}
+class SpotifyAnalyzer:
+    # create SpotifyAnalyzer instance
+    def __init__(self, username, redirect_uri='http://localhost:8888/callback', scope=["user-top-read", "playlist-read-private"]):
+        
+        self.token = None
+        self.sp = None
+        self.username = username
+        self.scope = scope
+        self.redirect_uri = redirect_uri
+        self.generate_token()
+        
+    # set/modify scope
+    def set_scope(self, scope):
+        self.scope = scope
+        self.generate_token()
+        
+    
+    def generate_token(self):
+        token = util.prompt_for_user_token(
+            self.username, 
+            self.scope, 
+            client_id=CLIENT_ID, 
+            client_secret=CLIENT_SECRET, 
+            redirect_uri=self.redirect_uri
+        )
+        if token:
+            self.sp = spotipy.Spotify(auth=token)
+            self.token = token
+        else:
+            print(f"Cant get token for {self.username}")
 
-# authorization flow
-token = util.prompt_for_user_token(
-    username, scope[0], 
-    client_id=CLIENT_ID, 
-    client_secret=CLIENT_SECRET, 
-    redirect_uri=redirect_uri
-)
+        
+    # print top artists
+    def get_top_artists(self):
+        # get request for top artists
+        headers = {
+            'Authorization': 'Bearer ' + self.token
+        }
+        params = {
+            "limit": 20,  # number of artists to retrieve
+            "time_range": "medium_term"  # time range for top artists (short_term, medium_term, long_term)
+        }
+        response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers, params=params)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-else:
-    print(f"Cant get token for {username}")
+        if response.status_code == 200:
+            data = response.json()
+        else:
+            print(f"Error: {response.status_code}")
 
-# get request for top artists
-headers = {
-    'Authorization': 'Bearer ' + token
-}
-params = {
-    "limit": 20,  # number of artists to retrieve
-    "time_range": "medium_term"  # time range for top artists (short_term, medium_term, long_term)
-}
-response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers, params=params)
-
-if response.status_code == 200:
-    data = response.json()
-else:
-    print(f"Error: {response.status_code}")
-
-# ANALYSIS
-data_str  = json.dumps(data, indent=2)
-data_dict = json.loads(data_str)
+        # ANALYSIS
+        data_str  = json.dumps(data, indent=2)
+        data_dict = json.loads(data_str)
 
 
-top_artists = data['items']
+        top_artists = data['items']
 
-for artist in top_artists:
-    artists = artist['name']
-    print(f"{artist['name']} Popularity: {artist['popularity']} Genres: {artist['genres']}")
+        for artist in top_artists:
+            artists = artist['name']
+            print(f"{artist['name']} Popularity: {artist['popularity']} Genres: {artist['genres']}")
+
+        return
+    
+    # print available genre seeds
+    def get_genre_seeds(self):
+        headers = {
+            'Authorization': 'Bearer ' + self.token
+        }
+        response2 = requests.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', headers=headers)
+
+        if response2.status_code == 200:
+            data2 = response2.json()
+        else:
+            print(f"Error: {response2.status_code}")
+
+        data2_str = json.dumps(data2, indent=2)
+        print(data2_str)
 
 
-response2 = requests.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', headers=headers)
+def main():
 
-if response2.status_code == 200:
-    data2 = response2.json()
-else:
-    print(f"Error: {response2.status_code}")
+    # credentials
+    username = 'nrkjbdqb3gwxlzypjce5vvqra'
+    redirect_uri = 'http://localhost:8888/callback'
 
-data2_str = json.dumps(data2, indent=2)
-print(data2_str)
+    # list of predefined scopes
+    scope = [
+        'user-top-read',
+        'user-read-recently-played',
+        'user-read-currently-playing', 
+        'playlist-read-private', 
+        'playlist-read-collaborative', 
+        'playlist-modify-private', 
+        'playlist-modify-public', 
+        'user-library-read'
+    ]
+
+    sp = SpotifyAnalyzer(username, redirect_uri)
+    sp.get_top_artists()
+    sp.get_genre_seeds()
+
+
+
+if __name__ == "__main__":
+    main()
